6769eec499e67672c40a02f42e5fbc3322dfbb17
Sannej <siitonenjenna@gmail.com>
2025-05-12 14:52:40 +0300

    pieni korjaus

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Person = require('./models/person'); // Person-malli
+const Person = require('./models/person');
 const app = express();
 const path = require('path');
 
@@ -89,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 // Palvelimen käynnistys
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

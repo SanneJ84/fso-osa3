3b078e505be0b89282ca9bce1a9661594002ce15
Sannej <siitonenjenna@gmail.com>
2025-05-12 14:23:22 +0300

    up

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Person = require('./models/person'); // Person-malli
 const app = express();
 const path = require('path');
-
 
 // Käytetään express.json(), jotta POST-pyynnön data on saatavilla
 app.use(express.json());
 app.use(cors());  // Käytetään CORSia, jotta voidaan tehdä pyyntöjä eri domaineista
-
 app.use(express.static('dist'));  // Käytetään staattista tiedostopalvelinta, jotta voimme palvella HTML-tiedostoja
 
 // Määritellään morganin loggaustyyli, joka näyttää myös requestin bodyn
@@ -17,61 +18,68 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 // Käytetään morgan middlewarea, joka loggaa kaikki saapuvat HTTP-pyynnöt
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  { id: "1", name: "Arto Hellas", number: "040-123456" },
-  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" },
-  { id: "5", name: "John Doe", number: "123-456-7890" }
-];
-
 // Reitit
+
+// Kaikkien henkilöiden hakeminen
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 });
 
+// Yksittäisen henkilön hakeminen ID:n perusteella
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).send({ error: 'Person not found' })
+    }
+  })
+});
+
+// Henkilön poistaminen
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id).then(result => {
+    if (result) {
+      response.status(204).end()
+    } else {
+      response.status(404).send({ error: 'Person not found' })
+    }
+  })
+});
+
+// Uuden henkilön lisääminen
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Name or number is missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+});
+
+// Tietoa puhelinluettelosta
 app.get('/api/info', (request, response) => {
   const date = new Date();
-  const info = `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`;
-  response.send(info);
+  Person.countDocuments({}).then(count => {
+    const info = `<p>Phonebook has info for ${count} people</p><p>${date}</p>`;
+    response.send(info);
+  })
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const personId = persons.find(person => person.id === id);
-  if (personId) {
-    response.json(personId);
-  } else {
-    response.status(404).send({ error: 'Person not found' });
-  }
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
-});
-
-app.post('/api/persons', (request, response) => {
-  const newPerson = request.body;
-  if (!newPerson.name || !newPerson.number) {
-    console.log('Name or number is missing');
-    return response.status(400).json({ error: 'Name or number is missing' });
-  }
-  const personExists = persons.some(person => person.name === newPerson.name);
-  if (personExists) {
-    console.log('Name must be unique');
-    return response.status(400).json({ error: 'Name must be unique' });
-  }
-  newPerson.id = (Math.random() * 1000).toFixed(2).toString();
-  persons.push(newPerson);
-  response.status(201).json(newPerson);
-});
-
+// Alkunäkymä
 app.get('/', (req, res) => {    
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 
 // Tuntemattomat reitit
 const unknownEndpoint = (request, response) => {
@@ -80,7 +88,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
+// Palvelimen käynnistys
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
